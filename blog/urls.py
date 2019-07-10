c53bd23064ae384d83955ce4137a38bccf169134
yinvahe <yin.vazquez@tveducativa.gob.mx>
2019-07-10 16:40:59 -0500

    Cambie el HTML para la pagina.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
