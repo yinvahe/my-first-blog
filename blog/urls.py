993c796e4fb3f5fa61564256ee9a07cf9c89a89d
yinvahe <yin.vazquez@tveducativa.gob.mx>
2019-07-10 17:14:58 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
